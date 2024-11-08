6a0ba90f9575f887321dc12876594b6c77146fa6
Bryson Camp <bryson@bryson.camp>
2024-11-08 23:21:24 +1000

    S3 Deploy Test 11

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ import AccountPage from "./pages/AccountPage";
 //import { Amplify } from 'aws-amplify';
 //import awsconfig from './aws-exports'; 
 
-//Amplify.configure(awsconfig);
 
 
 const AnimatedRoutes = () => {
